b63aed3ef32dda8f83610d20b120023cf2db347b
orache arthur <arthur.orache@gmail.com>
2019-04-14 22:29:41 +0300

    Add team model

---
+++ api/models/team.py
@@ -0,0 +1,13 @@
+from api.models import db, ModelMixin
+
+
+class Team(ModelMixin):
+    """
+    Team model attributes
+    """
+    __tablename__ = 'teams'
+
+    name = db.Column(db.String(120), nullable=False)
+    logo = db.Column(db.String(180))
+    league_position = db.Column(db.Integer)
+    manager = db.Column(db.String(180))

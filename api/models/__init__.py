fa0182ab6a3c19780b20f476bb12ca61095d7ebc
orache arthur <arthur.orache@gmail.com>
2019-04-14 22:41:57 +0300

    Add players model

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,2 +1,4 @@
 from .base_model import ModelMixin, db
 from .user import User
+from .team import Team
+from .players import Player
---
+++ api/models/players.py
@@ -0,0 +1,16 @@
+from api.models import db, ModelMixin
+
+
+class Player(ModelMixin):
+    """
+    Team model attributes
+    """
+    __tablename__ = 'players'
+
+    name = db.Column(db.String(120), nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
+    position = db.Column(db.String(180))
+    age = db.Column(db.Integer)
+    price = db.Column(db.Integer)
+    points = db.Column(db.Integer)
+    availability = db.Column(db.String(180))

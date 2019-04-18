57d8cdf73118973a38532764206e6240486a804a
orache arthur <arthur.orache@gmail.com>
2019-04-18 17:06:49 +0300

    Add user login

--- api/auth/__init__.py
+++ api/auth/__init__.py
@@ -1,11 +1,18 @@
 from flask import Blueprint
 
-from api.auth.views import RegisterView
+from api.auth.views import RegisterView, LoginView
 
 auth_blueprint = Blueprint('auth', __name__, url_prefix='/api/v1')
 registration_view = RegisterView.as_view('register_api')
 auth_blueprint.add_url_rule(
     '/auth/register',
     view_func=registration_view,
+    methods=['POST']
+)
+
+login_view = LoginView.as_view('login_api')
+auth_blueprint.add_url_rule(
+    '/auth/login',
+    view_func=login_view,
     methods=['POST']
 )

0ba2401067ea886a22921d3e2377fbcfe86c3700
orache arthur <arthur.orache@gmail.com>
2019-04-18 17:06:49 +0300

    Add user login

--- api/auth/views.py
+++ api/auth/views.py
@@ -63,3 +63,53 @@ class RegisterView(MethodView):
                 'message': 'User already exists. Please log in.',
             }
             return make_response(jsonify(response)), 409
+
+
+class LoginView(MethodView):
+    """
+    View to login the user
+    """
+
+    def post(self):
+        post_data = request.json
+        email = post_data.get('email')
+        password = post_data.get('password')
+
+        if not email or not password:
+            response = {
+                'status': 'fail',
+                'message': 'email or password not provided.'
+            }
+            return make_response(jsonify(response)), 400
+
+        if not validate_email(email):
+            response = {
+                'status': 'fail',
+                'message': 'Invalid email or password provided'
+            }
+            return make_response(jsonify(response)), 400
+
+        try:
+            user = User.find_first(email=email)
+            if user and user.password_is_valid(password):
+                auth_token = user.generate_token(user.id)
+                if auth_token:
+                    response = {
+                        'status': 'success',
+                        'message': 'Successfully logged in.',
+                        'auth_token': auth_token.decode()
+                    }
+                    return make_response(jsonify(response)), 200
+            else:
+                response = {
+                    'status': 'fail',
+                    'message': 'User does not exist.'
+                }
+                return make_response(jsonify(response)), 401
+        except Exception as e:
+            logging.error(f"An error has occurred  {e}")
+            response = {
+                'status': 'fail',
+                'message': 'An error has occurred. Please try again.'
+            }
+            return make_response(jsonify(response)), 500

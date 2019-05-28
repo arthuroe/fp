789a01bcfe400e5744a9055c72eddd11f06e2680
orache arthur <arthur.orache@gmail.com>
2019-05-28 17:50:45 +0300

    Add association for seasons

--- api/models/pivot_tables.py
+++ api/models/pivot_tables.py
@@ -5,3 +5,9 @@ db.Table('fantasyteam_players',
                    db.ForeignKey('fantasy_teams.id')),
          db.Column('player_id', db.Integer, db.ForeignKey('players.id'))
          )
+
+db.Table('season_teams',
+         db.Column('season_id', db.Integer,
+                   db.ForeignKey('seasons.id')),
+         db.Column('team_id', db.Integer, db.ForeignKey('teams.id'))
+         )

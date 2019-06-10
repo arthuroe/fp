7ed94834867378d3a2f82dee4d5a2c74083e7413
orache arthur <arthur.orache@gmail.com>
2019-06-10 11:56:22 +0300

    Add CORS

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 
 from config import app_configuration
 
@@ -9,6 +10,8 @@ app = Flask(__name__)
 environment = os.getenv("APP_SETTINGS")
 os.sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 app.config.from_object(app_configuration[environment])
+# add support for CORS for all end points
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 from api.models import db
 from api.auth import auth_blueprint

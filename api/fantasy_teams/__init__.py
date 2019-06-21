ff1dac4d38b5c773c54bb19dc6d3f31eb1f31901
orache arthur <arthur.orache@gmail.com>
2019-06-21 22:38:54 +0300

    Add create and view fantasy teams

--- api/fantasy_teams/__init__.py
+++ api/fantasy_teams/__init__.py
@@ -5,3 +5,5 @@ from .views import FantasyTeamView
 fantasy_team_blueprint = Blueprint(
     'fantasy_teams', __name__, url_prefix='/api/v1')
 fantasy_team_view = FantasyTeamView.as_view('fantasy_team_api')
+fantasy_team_blueprint.add_url_rule(
+    '/fantasy_teams', view_func=fantasy_team_view, methods=['GET', 'POST'])
--- api/fantasy_teams/views.py
+++ api/fantasy_teams/views.py
@@ -1,5 +1,73 @@
+import logging
+
+from flask import request, make_response, jsonify
 from flask.views import MethodView
+
+from api.decorators import token_required, admin_required
+from api.models import FantasyTeam
 
 
 class FantasyTeamView(MethodView):
-    pass
+    """
+    View to Fantasy Teams
+    """
+    decorators = [token_required]
+
+    def get(self, current_user, fantasy_team_id=None):
+        if fantasy_team_id:
+            fantasy_team = FantasyTeam.find_first(id=fantasy_team_id)
+            if not fantasy_team:
+                response = {
+                    'status': 'fail',
+                    'message': 'Fantasy Team does not exist'
+                }
+                return make_response(jsonify(response)), 400
+            response = {
+                'status': 'success',
+                'teams': fantasy_team.serialize()
+            }
+            return make_response(jsonify(response)), 200
+
+        fantasy_teams = FantasyTeam.fetch_all()
+        if not fantasy_teams:
+            response = {
+                'status': 'success',
+                'message': 'No seasons have been added'
+            }
+            return make_response(jsonify(response)), 200
+
+        response = {
+            'status': 'success',
+            'fantasy_teams': [fantasy_team.serialize() for fantasy_team in
+                              fantasy_teams]
+        }
+        return make_response(jsonify(response)), 200
+
+    def post(self, current_user):
+        kwargs = request.json
+        user_id = current_user.id
+        kwargs.update({"user_id": user_id})
+        name = request.json.get('name')
+
+        if not all([name]):
+            response = {
+                'status': 'fail',
+                'message': 'Incomplete data. All fields are required'
+            }
+            return make_response(jsonify(response)), 400
+
+        try:
+            fantasy_team = FantasyTeam(**kwargs)
+            fantasy_team.save()
+            response = {
+                'status': 'success',
+                'message': f'Successfully added {name}'
+            }
+            return make_response(jsonify(response)), 201
+        except Exception as e:
+            logging.error(f"An error has occurred  {e}")
+            response = {
+                'status': 'fail',
+                'message': 'Failed to add fantasy_team.'
+            }
+            return make_response(jsonify(response)), 400

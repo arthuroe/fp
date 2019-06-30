94536245887600046f9a266f06c94b425efe087c
orache arthur <arthur.orache@gmail.com>
2019-06-30 22:07:06 +0300

    Add view/create fantasy league

--- api/fantasy_leagues/__init__.py
+++ api/fantasy_leagues/__init__.py
@@ -5,3 +5,6 @@ from .views import FantasyLeagueView
 fantasy_league_blueprint = Blueprint(
     'fantasy_leagues', __name__, url_prefix='/api/v1')
 fantasy_league_view = FantasyLeagueView.as_view('fantasy_leagues_api')
+fantasy_league_blueprint.add_url_rule(
+    '/fantasy_leagues', view_func=fantasy_league_view, methods=['GET', 'POST']
+)

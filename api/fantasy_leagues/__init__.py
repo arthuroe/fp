ab9825a5a5faf5ff5df8d5e99de7209a5d9bf4ca
orache arthur <arthur.orache@gmail.com>
2019-07-07 18:03:54 +0300

    Add fantasy team to league

--- api/fantasy_leagues/__init__.py
+++ api/fantasy_leagues/__init__.py
@@ -1,10 +1,16 @@
 from flask import Blueprint
 
-from .views import FantasyLeagueView
+from .views import FantasyLeagueView, FantasyLeagueUsersView
 
 fantasy_league_blueprint = Blueprint(
     'fantasy_leagues', __name__, url_prefix='/api/v1')
 fantasy_league_view = FantasyLeagueView.as_view('fantasy_leagues_api')
 fantasy_league_blueprint.add_url_rule(
     '/fantasy_leagues', view_func=fantasy_league_view, methods=['GET', 'POST']
+)
+
+fantasy_league_user_view = FantasyLeagueUsersView.as_view(
+    'fantasy_leagues_users_api')
+fantasy_league_blueprint.add_url_rule(
+    '/join_fantasy_league', view_func=fantasy_league_user_view, methods=['POST']
 )

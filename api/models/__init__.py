de1663a192281da268487c10a6eb5508adfdab78
orache arthur <arthur.orache@gmail.com>
2019-07-26 16:03:51 +0300

    Fix issues with articles

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,4 +1,5 @@
 from .base_model import ModelMixin, db
+from .articles import Article
 from .fantasy_league import FantasyLeague
 from .fantasyleague_team import FantasyLeagueTeam
 from .fantasy_team import FantasyTeam

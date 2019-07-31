8a50b704e301c829f1679c3deacdec94a9638404
orache arthur <arthur.orache@gmail.com>
2019-07-31 15:14:57 +0300

    Add routes

--- api/fantasy_teams/__init__.py
+++ api/fantasy_teams/__init__.py
@@ -4,6 +4,7 @@ from .views import FantasyTeamView, PlayerFantasyTeamView
 
 fantasy_team_blueprint = Blueprint(
     'fantasy_teams', __name__, url_prefix='/api/v1')
+
 fantasy_team_view = FantasyTeamView.as_view('fantasy_team_api')
 fantasy_team_blueprint.add_url_rule(
     '/fantasy_teams', view_func=fantasy_team_view, methods=['GET', 'POST'])
@@ -11,6 +12,23 @@ fantasy_team_blueprint.add_url_rule(
 fantasy_team_players_view = PlayerFantasyTeamView.as_view(
     'player_fantasy_team_api')
 fantasy_team_blueprint.add_url_rule(
-    '/fantasy_team_players/<fantasy_team_id>', view_func=fantasy_team_players_view,
+    '/fantasy_team_players/<fantasy_team_id>',
+    view_func=fantasy_team_players_view,
+    methods=['GET', 'POST']
+)
+
+fantasy_team_captain_view = PlayerFantasyTeamView.as_view(
+    'fantasy_team_captain_api')
+fantasy_team_blueprint.add_url_rule(
+    '/fantasy_team_captain/<player_id>',
+    view_func=fantasy_team_captain_view,
+    methods=['GET', 'POST']
+)
+
+fantasy_team_vice_captain_view = PlayerFantasyTeamView.as_view(
+    'fantasy_team_vice_captain_api')
+fantasy_team_blueprint.add_url_rule(
+    '/fantasy_team_vice_captain/<player_id>',
+    view_func=fantasy_team_vice_captain_view,
     methods=['GET', 'POST']
 )

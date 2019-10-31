383f185448a7b87415e100976d72d345f213d814
orache arthur <arthur.orache@gmail.com>
2019-10-31 11:05:50 +0300

    Setup fixtures blueprint

---
+++ api/fixtures/vews.py
@@ -0,0 +1,13 @@
+from flask.views import MethodView
+
+
+class FixturesView(MethodView):
+    """
+    View to handle Players
+    """
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass

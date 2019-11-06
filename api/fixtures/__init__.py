351b752e92668b70c6e32b252093847607ca3450
orache arthur <arthur.orache@gmail.com>
2019-11-06 21:35:07 +0300

    Add post fixture

--- api/fixtures/__init__.py
+++ api/fixtures/__init__.py
@@ -5,6 +5,6 @@ from .views import FixturesView
 fixtures_blueprint = Blueprint('fixtures', __name__, url_prefix='/api/v1')
 fixtures_view = FixturesView.as_view('fixtures_api')
 fixtures_blueprint.add_url_rule(
-    '/fixtures', view_func=fixtures_view, methods=['GET', 'POST'])
+    '/fixtures', view_func=fixtures_view, methods=['GET'])
 fixtures_blueprint.add_url_rule(
-    '/fixtures/<season_id>', view_func=fixtures_view, methods=['GET'])
+    '/fixtures/<season_id>', view_func=fixtures_view, methods=['GET', 'POST'])

313c182464c2f94247dd967505f8578890d21b5d
orache arthur <arthur.orache@gmail.com>
2019-11-12 20:32:05 +0300

    Add post gameweek

--- api/game_week/__init__.py
+++ api/game_week/__init__.py
@@ -5,5 +5,10 @@ from .views import GameWeekView
 game_week_blueprint = Blueprint('game_weeks', __name__, url_prefix='/api/v1')
 game_week_view = GameWeekView.as_view('game_week_api')
 game_week_blueprint.add_url_rule(
-    '/game_week', view_func=game_week_view, methods=['GET', 'POST']
+    '/game_week/<season_id>/<game_week_id>', view_func=game_week_view,
+    methods=['GET', 'POST']
+)
+game_week_blueprint.add_url_rule(
+    '/game_week/<season_id>', view_func=game_week_view,
+    methods=['GET', 'POST']
 )

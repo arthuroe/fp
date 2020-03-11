0325fccc44b60ac5f6b09b3f9d86881671d8d545
orache arthur <arthur.orache@gmail.com>
2020-03-11 13:18:38 +0300

    Add update fantasy team name

--- api/fantasy_teams/__init__.py
+++ api/fantasy_teams/__init__.py
@@ -8,6 +8,10 @@ fantasy_team_blueprint = Blueprint(
 fantasy_team_view = FantasyTeamView.as_view('fantasy_team_api')
 fantasy_team_blueprint.add_url_rule(
     '/fantasy_teams', view_func=fantasy_team_view, methods=['GET', 'POST'])
+fantasy_team_blueprint.add_url_rule(
+    '/fantasy_teams/<fantasy_team_id>', view_func=fantasy_team_view,
+    methods=['GET', 'PUT']
+)
 
 fantasy_team_players_view = PlayerFantasyTeamView.as_view(
     'player_fantasy_team_api')

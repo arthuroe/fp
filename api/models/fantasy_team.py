c777ffc570205f1f620cb1b50ad2f6aa446fb4c9
orache arthur <arthur.orache@gmail.com>
2020-03-25 01:07:45 +0300

    Set default points and team money

--- api/models/fantasy_team.py
+++ api/models/fantasy_team.py
@@ -9,8 +9,8 @@ class FantasyTeam(ModelMixin):
 
     name = db.Column(db.String(120), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    points = db.Column(db.Integer)
-    money = db.Column(db.Integer)
+    points = db.Column(db.Integer, default=0)
+    money = db.Column(db.Integer, default=100)
     season_id = db.Column(db.Integer, db.ForeignKey(
         'seasons.id'), nullable=False)
     captain = db.Column(db.Integer)

68a03f3bb330d554041a151ab6090204a6c4ebf6
orache arthur <arthur.orache@gmail.com>
2020-03-26 23:47:06 +0300

    Modify fantasy team player model

---
+++ api/models/fantasy_team_players.py
@@ -0,0 +1,13 @@
+from api.models import db, ModelMixin
+
+
+class FantasyTeamPlayers(ModelMixin):
+    """
+    Fantasy Team players model attributes
+    """
+    __tablename__ = 'fantasy_team_players'
+
+    fantasyteam_id = db.Column(
+        'fantasyteam_id', db.Integer, db.ForeignKey('fantasy_teams.id'))
+    player_id = db.Column('player_id', db.Integer, db.ForeignKey('players.id'))
+    is_sub = db.Column(db.Boolean, default=False)

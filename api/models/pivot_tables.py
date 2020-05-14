7d8769a2655517fd7260e3f2b0b81b5ac5a3cd79
orache arthur <arthur.orache@gmail.com>
2020-05-14 03:42:06 +0300

    Add uuid codes to db

--- api/models/pivot_tables.py
+++ api/models/pivot_tables.py
@@ -1,11 +1,5 @@
 from api.models import db
 
-db.Table('fantasyteam_players',
-         db.Column('fantasyteam_id', db.Integer,
-                   db.ForeignKey('fantasy_teams.id')),
-         db.Column('player_id', db.Integer, db.ForeignKey('players.id'))
-         )
-
 db.Table('season_teams',
          db.Column('season_id', db.Integer, db.ForeignKey('seasons.id')),
          db.Column('team_id', db.Integer, db.ForeignKey('teams.id'))

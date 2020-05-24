ecc26d0361d56814e5f59abe9e2f28b8459a67cb
orache arthur <arthur.orache@gmail.com>
2020-05-24 22:24:53 +0300

    Add player details to fantasy team players

--- api/fantasy_teams/helpers.py
+++ api/fantasy_teams/helpers.py
@@ -1,3 +1,6 @@
+from api.models import Player
+
+
 def check_max_players_from_team(player, fantasy_team):
     teams = [player.team_id for player in fantasy_team]
     frequency = {team: teams.count(team) for team in teams}
@@ -8,3 +11,15 @@ def check_max_players_from_team(player, fantasy_team):
     for team_id in frequency:
         if team_id == player.team_id:
             return False if frequency[team_id] >= 3 else True
+
+
+def add_player_info(fantasy_players):
+    for fantasy_player in fantasy_players:
+        player = Player.find_first(id=fantasy_player.get('player_id'))
+        jersey = player.team.jersey
+        player = player.serialize()
+        del player["id"]
+        del player["uuid"]
+        fantasy_player.update(player)
+        fantasy_player.update({"jersey": jersey})
+    return fantasy_players

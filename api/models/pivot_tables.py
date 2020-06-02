b436c44015e265daa8653068766c8407476cc6ef
orache arthur <arthur.orache@gmail.com>
2020-06-02 13:23:14 +0300

    Add user attributes

--- api/models/pivot_tables.py
+++ api/models/pivot_tables.py
@@ -4,3 +4,8 @@ db.Table('season_teams',
          db.Column('season_id', db.Integer, db.ForeignKey('seasons.id')),
          db.Column('team_id', db.Integer, db.ForeignKey('teams.id'))
          )
+
+db.Table('user_teams',
+         db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+         db.Column('team_id', db.Integer, db.ForeignKey('teams.id'))
+         )

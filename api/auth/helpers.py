2f87037aa92d620513c303f9c28f709d2b9bbdf0
orache arthur <arthur.orache@gmail.com>
2020-06-05 22:58:20 +0300

    Add reset password

--- api/auth/helpers.py
+++ api/auth/helpers.py
@@ -1,5 +1,9 @@
 import re
 
+from api import app
+
+from itsdangerous import URLSafeTimedSerializer
+
 
 def validate_email(email):
     if re.search('[^@]+@[^@]+\.[^@]+', email):
@@ -14,3 +18,25 @@ def validate_password(password):
     DIGIT = re.compile(r'[0-9]')
     ALL_PATTERNS = (LENGTH, UPPERCASE, LOWERCASE, DIGIT)
     return all(pattern.search(password) for pattern in ALL_PATTERNS)
+
+
+def generate_reset_url(base_url, token):
+    return f"http://{base_url}/api/v1/auth/reset_password/{token}"
+
+
+def generate_confirmation_token(email):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
+
+
+def confirm_token(token, expiration=3600):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    try:
+        email = serializer.loads(
+            token,
+            salt=app.config['SECURITY_PASSWORD_SALT'],
+            max_age=expiration
+        )
+    except:
+        return False
+    return email
--- api/auth/views.py
+++ api/auth/views.py
@@ -1,10 +1,13 @@
 import logging
 
-from flask import Blueprint, request, make_response, jsonify
+from flask import (
+    Blueprint, request, redirect, make_response, jsonify, render_template)
 from flask.views import MethodView
+from flask.wrappers import Response
 
 from api.auth.helpers import *
 from api.decorators import token_required
+from api.email import send_email
 from api.models import User
 
 
@@ -16,10 +19,10 @@ class RegisterView(MethodView):
     def post(self):
         post_data = request.json
         email = post_data.get('email')
-        name = post_data.get('name')
+        first_name = post_data.get('first_name')
         password = post_data.get('password')
 
-        if not all([email, name, password]):
+        if not all([email, first_name, password]):
             response = {
                 'status': 'fail',
                 'message': ('Incomplete data. email, name and '
@@ -30,7 +33,7 @@ class RegisterView(MethodView):
         if not validate_email(email) or not validate_password(password):
             response = {
                 'status': 'fail',
-                'message': ('Invalid Email or password provided'),
+                'message': 'Invalid Email or password provided',
                 'required': (
                     'Passwords should be at least 8 characters, contain'
                     ' a digit, uppercase and lowercase characters'
@@ -41,7 +44,7 @@ class RegisterView(MethodView):
         user = User.find_first(email=post_data.get('email'))
         if not user:
             try:
-                user = User(email=email, password=password, name=name)
+                user = User(**post_data)
                 user.save()
 
                 response = {
@@ -118,5 +121,112 @@ class LoginView(MethodView):
             response = {
                 'status': 'fail',
                 'message': 'An error has occurred. Please try again.'
+            }
+            return make_response(jsonify(response)), 500
+
+
+class ChangePasswordView(MethodView):
+    """
+    View to change user password
+    """
+    decorators = [token_required]
+
+    def post(self, current_user):
+        kwargs = request.json
+        current_password = kwargs.get('current_password')
+        new_password = kwargs.get('new_password')
+
+        try:
+            user = User.find_first(id=current_user.id)
+
+            if not validate_password(new_password):
+                response = {
+                    'status': 'fail',
+                    'message': 'Invalid password provided',
+                    'required': (
+                        'Passwords should be at least 8 characters, contain'
+                        ' a digit, uppercase and lowercase characters'
+                    )
+                }
+                return make_response(jsonify(response)), 400
+
+            if not user.password_is_valid(current_password):
+                response = {
+                    'status': 'fail',
+                    'message': 'Invalid current password provided.'
+                }
+                return make_response(jsonify(response)), 400
+
+            if current_password == new_password:
+                response = {
+                    'status': 'fail',
+                    'meassge': 'Current password and new password should not match.'
+                }
+                return make_response(jsonify(response)), 400
+
+            password = user.hash_password(new_password)
+            user.update(password=password, id=current_user.id)
+            response = {
+                'status': 'success',
+                'meassge': 'Password successfully updated.'
+            }
+            return make_response(jsonify(response)), 201
+
+        except Exception as e:
+            logging.error(f"An error has occurred  {e}")
+            response = {
+                'status': 'fail',
+                'message': 'Failed to update password please try again.'
+            }
+            return make_response(jsonify(response)), 500
+
+
+class ResetPasswordView(MethodView):
+    """
+    View to reset user password
+    """
+
+    def get(self, token):
+        if confirm_token(token):
+            email = confirm_token(token)
+            user = User.find_first(email=email)
+            token = user.generate_token(user.id)
+            return (
+                "", 302,
+                {
+                    "location": "https://fantasy-256.herokuapp.com/",
+                    "Authorization": token
+                }
+            )
+
+        response = {
+            'status': 'fail',
+            'meassge': 'Expired or invalid url.'
+        }
+        return make_response(jsonify(response)), 400
+
+    def post(self):
+        email = request.json.get('email')
+        try:
+            user = User.find_first(email=email)
+            if not user:
+                return make_response(jsonify({'message': 'No user information found'})), 404
+
+            token = generate_confirmation_token(email)
+            url = generate_reset_url(request.headers.get('host'), token)
+            html = render_template(
+                'reset.html', username=user.first_name, url=url)
+            subject = "Reset your password"
+            send_email(subject, [user.email], 'Reset Password', html)
+            response = {
+                'status': 'success',
+                'meassge': 'Instructions have been sent to your email.'
+            }
+            return make_response(jsonify(response)), 200
+        except Exception as e:
+            logging.error(f"An error has occurred  {e}")
+            response = {
+                'status': 'fail',
+                'message': 'Failed to send email.'
             }
             return make_response(jsonify(response)), 500

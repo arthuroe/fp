c4d8dc69c1f23f0b11ffb95f9e385e5b6c847f62
orache arthur <arthur.orache@gmail.com>
2020-06-05 22:58:20 +0300

    Add reset password

--- api/auth/__init__.py
+++ api/auth/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 
-from api.auth.views import RegisterView, LoginView
+from api.auth.views import (
+    RegisterView, LoginView, ChangePasswordView, ResetPasswordView)
 
 auth_blueprint = Blueprint('auth', __name__, url_prefix='/api/v1')
 registration_view = RegisterView.as_view('register_api')
@@ -15,4 +16,24 @@ auth_blueprint.add_url_rule(
     '/auth/login',
     view_func=login_view,
     methods=['POST']
+)
+
+change_password_view = ChangePasswordView.as_view('change_password_api')
+auth_blueprint.add_url_rule(
+    '/auth/change_password',
+    view_func=change_password_view,
+    methods=['POST']
+)
+
+reset_password_view = ResetPasswordView.as_view('reset_password_api')
+auth_blueprint.add_url_rule(
+    '/auth/reset_password',
+    view_func=reset_password_view,
+    methods=['POST']
+)
+
+auth_blueprint.add_url_rule(
+    '/auth/reset_password/<token>',
+    view_func=reset_password_view,
+    methods=['GET']
 )

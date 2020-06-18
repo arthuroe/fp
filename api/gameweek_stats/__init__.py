05dbf539ba859ea51b1a56f857347326d292e770
orache arthur <arthur.orache@gmail.com>
2020-06-18 21:39:25 +0300

    Add view for team of the week

--- api/gameweek_stats/__init__.py
+++ api/gameweek_stats/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from .views import GameWeekStatsView
+from .views import GameWeekStatsView, TeamOfWeekView
 
 gameweek_stats_blueprint = Blueprint(
     'gameweek_stats', __name__, url_prefix='/api/v1')
@@ -16,4 +16,11 @@ gameweek_stats_blueprint.add_url_rule(
 gameweek_stats_blueprint.add_url_rule(
     '/gameweek_stats/<game_week_id>/<player_id>', view_func=gameweek_stats_view,
     methods=['GET', 'POST', 'PUT', 'DELETE']
+)
+
+team_of_the_week_view = TeamOfWeekView.as_view('team_of_the_week_api')
+gameweek_stats_blueprint.add_url_rule(
+    '/gameweek_stats/<game_week_id>/team_of_the_week',
+    view_func=team_of_the_week_view,
+    methods=['GET']
 )
--- api/gameweek_stats/views.py
+++ api/gameweek_stats/views.py
@@ -3,7 +3,7 @@ import logging
 from flask import request, make_response, jsonify
 from flask.views import MethodView
 
-from .helpers import award_points, get_fantasy_player_stats
+from .helpers import *
 from api.decorators import admin_required, token_required
 from api.models import PlayerGameWeek, GameWeek, FantasyTeam
 
@@ -136,3 +136,58 @@ class GameWeekStatsView(MethodView):
                 'message': 'Failed to update gameweek stats.'
             }
             return make_response(jsonify(response)), 500
+
+
+class TeamOfWeekView(MethodView):
+    "View to handle team of the week"
+    decorators = [token_required]
+
+    def get(self, current_user, game_week_id):
+        game_week = GameWeek.find_first(id=game_week_id)
+        if not game_week:
+            response = {
+                'status': 'fail',
+                'message': 'GameWeek does not exist'
+            }
+            return make_response(jsonify(response)), 404
+
+        players = PlayerGameWeek.filter_by(game_week_id=game_week_id)
+
+        team_of_the_week = []
+
+        if players:
+            team_of_the_week.extend(
+                get_top_players_in_category(players, 'FR', 3))
+            team_of_the_week.extend(
+                get_top_players_in_category(players, 'SR', 2))
+            team_of_the_week.extend(
+                get_top_players_in_category(players, 'BR', 3))
+            team_of_the_week.extend(
+                get_top_players_in_category(players, 'HB', 2))
+            team_of_the_week.extend(
+                get_top_players_in_category(players, 'C', 2))
+            team_of_the_week.extend(
+                get_top_players_in_category(players, 'WB', 3))
+
+        if team_of_the_week:
+            team = []
+            for player_stats in team_of_the_week:
+                info = player_stats.player_info
+                player_stats = player_stats.serialize()
+                player_info = info.serialize()
+                player_info['jersey'] = info.team.jersey
+                player_info['player_stats'] = player_stats
+                team.append(player_info)
+            team_of_the_week = team
+
+        response = {
+            'status': 'success',
+            'team': team_of_the_week
+        }
+        return make_response(jsonify(response)), 200
+
+        response = {
+            'status': 'success',
+            'team': 'No stats have been provided for this yet'
+        }
+        return make_response(jsonify(response)), 200

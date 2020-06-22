3e36634d5751756411ad0bec1904d3e6805bdec5
orache arthur <arthur.orache@gmail.com>
2020-06-22 17:14:39 +0300

    Add default player price

--- api/models/players.py
+++ api/models/players.py
@@ -11,7 +11,7 @@ class Player(ModelMixin):
     team_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
     position = db.Column(db.String(180))
     age = db.Column(db.Integer)
-    price = db.Column(db.Integer)
+    price = db.Column(db.Integer, default=8)
     points = db.Column(db.Integer)
     availability = db.Column(db.String(180))
     fantasy_teams = db.relationship(

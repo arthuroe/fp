970e14bb3f7ac9cbc43fc5014fd25bd699b06e4b
orache arthur <arthur.orache@gmail.com>
2020-07-02 10:57:11 +0300

    Add results attribute for fixture

--- api/models/fixtures.py
+++ api/models/fixtures.py
@@ -12,5 +12,7 @@ class Fixture(ModelMixin):
     date = db.Column(db.DateTime, nullable=False)
     home_team = db.Column(db.String(180))
     away_team = db.Column(db.String(180))
+    home_team_result = db.Column(db.Integer)
+    away_team_result = db.Column(db.Integer)
     game_week_id = db.Column(db.Integer, db.ForeignKey(
         'gameweeks.id'), nullable=False)

7cb507bc19ba66b107ebf44c2ff9c74f68a715d3
orache arthur <arthur.orache@gmail.com>
2020-09-15 01:25:01 +0300

    Fix fixture stats relationship

--- api/models/fixtures.py
+++ api/models/fixtures.py
@@ -10,8 +10,8 @@ class Fixture(ModelMixin):
     __tablename__ = 'fixtures'
 
     date = db.Column(db.DateTime, nullable=False)
-    home_team = db.Column(db.String(180))
-    away_team = db.Column(db.String(180))
+    home_team_id = db.Column(db.Integer, nullable=False)
+    away_team_id = db.Column(db.Integer, nullable=False)
     home_team_result = db.Column(db.Integer)
     away_team_result = db.Column(db.Integer)
     game_week_id = db.Column(db.Integer, db.ForeignKey(

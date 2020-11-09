6da71322f6d8a81e26ba778d5e908f6ef040167b
orache arthur <arthur.orache@gmail.com>
2020-11-09 17:46:07 +0300

    Add team info to fixture response

--- api/fixtures/helpers.py
+++ api/fixtures/helpers.py
@@ -1,3 +1,6 @@
+from api.models import Team
+
+
 def get_player_stats(players, fixture):
     home_team, away_team = [], []
     for player in players:
@@ -9,3 +12,16 @@ def get_player_stats(players, fixture):
         if fixture.away_team_id == player.player_info.team.id:
             away_team.append(serialized_player)
     return home_team, away_team
+
+
+def get_team_info(fixture):
+    home_team_id = fixture['home_team_id']
+    away_team_id = fixture['away_team_id']
+    home_team = Team.find_first(id=home_team_id)
+    away_team = Team.find_first(id=away_team_id)
+    fixture.update(
+        {"home_team": home_team.serialize(), "away_team": away_team.serialize()}
+    )
+    del fixture['home_team_id']
+    del fixture['away_team_id']
+    return fixture

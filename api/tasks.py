397db0d7919c008704cc7c630610caf288aedbea
orache arthur <arthur.orache@gmail.com>
2023-04-30 13:16:12 +0300

    Set default if no date

--- api/tasks.py
+++ api/tasks.py
@@ -11,15 +11,31 @@ def update_current_game_week():
         current_season = Season.find_first(is_current=True)
         game_weeks = current_season.gameweeks.order_by(
             GameWeek.date.asc()).all()
+        
+        game_weeks.sort(key=lambda game_week:int(game_week.name))
+        found = False
+
         for game_week in game_weeks:
+            print(game_week, type(game_week))
             game_week.is_current = False
+            
             if check_date(game_week.date):
+                # Set gameweek equal to current date to true
                 game_week.is_current = True
+                found = True
             game_week.save()
+
+            if found:
+                break
+
+        if not found:
+            # set to first gameweek
+            game_weeks[0].is_current = True
+            game_weeks[0].save()
 
         # Using print for logging because Gunicorn is not displaying logs \
         # from logging module
-        print("Updated GameWeeks..")
+        print("Updated GameWeeks......")
 
 
 def check_date(date):
